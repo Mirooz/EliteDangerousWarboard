1e711d41015119cebc1f6e4a05426ed975534f42
Mirooz <ewen_gallet@hotmail.com>
2025-12-30 00:15:43 +0100

    Update `NavRouteOverlayComponent` to adjust minimum overlay height to 100 for improved layout consistency.

--- elite-warboard-missions/src/main/java/be/mirooz/elitedangerous/dashboard/view/exploration/NavRouteOverlayComponent.java
+++ elite-warboard-missions/src/main/java/be/mirooz/elitedangerous/dashboard/view/exploration/NavRouteOverlayComponent.java
@@ -38,7 +38,7 @@ public class NavRouteOverlayComponent {
 
     public static final double MIN_OPACITY = 0.01;
     public static final int MIN_WIDTH_OVERLAY = 600;
-    public static final int MIN_HEIGHT_OVERLAY = 90;
+    public static final int MIN_HEIGHT_OVERLAY = 100;
     private final PreferencesService preferencesService = PreferencesService.getInstance();
     private final PopupManager popupManager = PopupManager.getInstance();
     private final LocalizationService localizationService = LocalizationService.getInstance();
