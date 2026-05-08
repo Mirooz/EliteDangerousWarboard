6044b59013e78759eaaed6c1fac988b8746b64b8
Mirooz <ewen_gallet@hotmail.com>
2026-05-08 13:38:06 +0200

    Add batch loading check to ShipTargetedHandler to conditionally process events

--- elite-warboard-missions/src/main/java/be/mirooz/elitedangerous/dashboard/handlers/events/journalevents/ShipTargetedHandler.java
+++ elite-warboard-missions/src/main/java/be/mirooz/elitedangerous/dashboard/handlers/events/journalevents/ShipTargetedHandler.java
@@ -2,6 +2,7 @@ package be.mirooz.elitedangerous.dashboard.handlers.events.journalevents;
 
 import be.mirooz.elitedangerous.dashboard.service.AutoPowerplantKeyService;
 import be.mirooz.elitedangerous.dashboard.service.ShipTargetService;
+import be.mirooz.elitedangerous.dashboard.view.common.context.DashboardContext;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class ShipTargetedHandler implements JournalEventHandler {
@@ -16,7 +17,9 @@ public class ShipTargetedHandler implements JournalEventHandler {
     @Override
     public void handle(JsonNode jsonNode) {
         try {
-            autoPowerplantKeyService.onShipTargeted(jsonNode);
+            if (!DashboardContext.getInstance().isBatchLoading()) {
+                autoPowerplantKeyService.onShipTargeted(jsonNode);
+            }
             shipTargetService.registerTarget(jsonNode);
         } catch (Exception e) {
             System.err.println("Erreur lors du parsing de Bounty: " + e.getMessage());
